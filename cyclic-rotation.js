84f7888c6380d08f56469c0bdb74ed6eb87f8497
Angarsk8 <andresa.garciah621@gmail.com>
2017-02-16 01:50:55 -0500

    add solution for the CyclicRotation problem

---
+++ cyclic-rotation.js
@@ -0,0 +1,17 @@
+function solution(A, K) {
+  const len = A.length
+  const shifts = K % len
+
+  if (shifts === 0) {
+    return A
+  }
+
+  return A.reduce((acc, value, i) => {
+    if (i + shifts + 1 > len) {
+      acc[(shifts + i) - len] = value
+    } else {
+      acc[i + shifts] = value
+    }
+    return acc
+  }, Array(len))
+}
